a26cefa95e6e05c30d3425397778978647d5022b
whitesocks <17552637+whites0cks@users.noreply.github.com>
2023-11-30 10:16:57 +0100

    Create activatecookie.js

---
+++ activatecookie.js
@@ -0,0 +1,5 @@
+fetch('http://localhost/activate-plugin/1', {
+  headers: {
+    Cookie: 'session=.eJwljjkOwzAMwP7iuYPlQ7LzmUInmiEZkmYq-vcG6EYOBPhJzzj8fKXlfVz-SM_V0pKMqlYtE4r45GCHEZSzzaDOnVtD1AFSOwyMTCAdGoIHEUqW2XhEs8ESpjxlKhTX0vOda5PmEQW6cbZA6qRjaClk7jUGzYqK6R65Tj_-N3DrzpvfyLate_r-AOkZNLA.ZWhRiw.eQIZWTowm8S9vymPYvtOvOOyBQI'
+  }
+})
